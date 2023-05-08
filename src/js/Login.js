7e73d105a6790155f6afa289622225777222a70d
HeroutRozilyo <heroot12@gmail.com>
2023-05-08 12:36:27 +0300

    gbh

--- src/js/Login.js
+++ src/js/Login.js
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link, useHistory } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link, useNavigate } from 'react-router-dom';
 
 function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const history = useHistory();
+  const history = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route,
   Link,
   useHistory,
 } from 'react-router-dom';
 
-import Login from '../js/Login.js'
-import Application from '../js/Application.js'
+
 function main() {
     return (
-    <Router>
-    <div>
-    <Switch>
-    <Route exact path="/">
-    <Login />
-    </Route>
-    <Route path="/application">
-    <Application />
-    </Route>
-    </Switch>
-    </div>
-    </Router>
+    <h1>Helloooooo</h1>
     );
     }
     
