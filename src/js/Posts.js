1ad60a857434a4fa4b528d99a3fab04b5ee2e546
HeroutRozilyo <heroot12@gmail.com>
2023-05-08 22:09:07 +0300

    posts

--- src/js/Posts.js
+++ src/js/Posts.js
@@ -1,26 +1,53 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+
 function Posts() {
-    const [posts, setPosts] = useState([]);
-    
-    useEffect(() => {
-    const fetchPosts = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const data = await response.json();
-    setPosts(data);
-    };
-    fetchPosts();
-    }, []);
-    
-    return (
+  const [posts, setPosts] = useState([]);
+  const [selectedPost, setSelectedPost] = useState(null);
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    const userId = user.id;
+    fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+      .then(response => response.json())
+      .then(data => setPosts(data))
+      .catch(error => console.log(error));
+  }, []);
+
+  const handlePostClick = (postId) => {
+    setSelectedPost(postId);
+    fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
+      .then(response => response.json())
+      .then(data => setComments(data))
+      .catch(error => console.log(error));
+  }
+
+  return (
     <div>
-    <h2>Posts</h2>
-    {posts.map((post) => (
-    <div key={post.id}>
-    <h3>{post.title}</h3>
-    <p>{post.body}</p>
+      <h2>Posts</h2>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id} className={selectedPost === post.id ? 'selected' : ''}>
+            <button onClick={() => handlePostClick(post.id)}>View</button>
+            {post.title}
+            {selectedPost === post.id && (
+              <div>
+                <h3>Comments</h3>
+                <ul>
+                  {comments.map(comment => (
+                    <li key={comment.id}>
+                      <strong>{comment.name}</strong>
+                      <p>{comment.body}</p>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </li>
+        ))}
+      </ul>
     </div>
-    ))}
-    </div>
-    );
-    }
-    export default Posts; 
+  );
+}
+
+export default Posts;
