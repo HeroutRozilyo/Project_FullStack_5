8fd72feda049a1a3157d0da7aa96aeeffeea9ff8
HeroutRozilyo <heroot12@gmail.com>
2023-05-08 22:21:27 +0300

    Album beggin

--- src/js/Album.js
+++ src/js/Album.js
@@ -1,25 +1,57 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+
 function Albums() {
-    const [albums, setAlbums] = useState([]);
-    
-    useEffect(() => {
-    const fetchAlbums = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/albums');
-    const data = await response.json();
-    setAlbums(data);
-    };
-    fetchAlbums();
-    }, []);
-    
-    return (
+  const [albums, setAlbums] = useState([]);
+  const [selectedAlbum, setSelectedAlbum] = useState(null);
+  const [photos, setPhotos] = useState([]);
+  const [start, setStart] = useState(0);
+  const [end, setEnd] = useState(10);
+  const user = JSON.parse(localStorage.getItem('user'));
+  const userId = user.id;
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/albums?userId=${userId}`)
+      .then(response => response.json())
+      .then(data => setAlbums(data))
+      .catch(error => console.log(error));
+  }, [userId]);
+
+  const handleAlbumClick = (albumId) => {
+    setSelectedAlbum(albumId);
+    setStart(0);
+    setEnd(10);
+    fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`)
+      .then(response => response.json())
+      .then(data => setPhotos(data))
+      .catch(error => console.log(error));
+  }
+
+  const handleLoadMore = () => {
+    setStart(start + 10);
+    setEnd(end + 10);
+  }
+
+  return (
     <div>
-    <h2>Albums</h2>
-    {albums.map((album) => (
-    <div key={album.id}>
-    <h3>{album.title}</h3>
+      <h2>Albums</h2>
+      <ul>
+        {albums.map(album => (
+          <li key={album.id} className={selectedAlbum === album.id ? 'selected' : ''}>
+            <button onClick={() => handleAlbumClick(album.id)}>View</button>
+            {album.title}
+            {selectedAlbum === album.id && (
+              <div className="photo-list">
+                {photos.slice(start, end).map(photo => (
+                  <img key={photo.id} src={photo.thumbnailUrl} alt={photo.title} />
+                ))}
+                {end < photos.length && <button onClick={handleLoadMore}>Load More</button>}
+              </div>
+            )}
+          </li>
+        ))}
+      </ul>
     </div>
-    ))}
-    </div>
-    );
-    }
-    export default Albums; 
+  );
+}
+
+export default Albums;
