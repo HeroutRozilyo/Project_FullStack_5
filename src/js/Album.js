e55491b4aa035e434cdf5a6204fde263ac44223b
da077 <da077@DESKTOP-HV19KNK>
2023-05-09 11:52:41 +0300

    albumsPosts

--- src/js/Album.js
+++ src/js/Album.js
@@ -6,45 +6,68 @@ function Albums() {
   const [photos, setPhotos] = useState([]);
   const [start, setStart] = useState(0);
   const [end, setEnd] = useState(10);
-  const user = JSON.parse(localStorage.getItem('user'));
+  const [showAlbums, setShowAlbums] = useState(false);
+  const user = JSON.parse(localStorage.getItem("user"));
   const userId = user.id;
 
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/albums?userId=${userId}`)
-      .then(response => response.json())
-      .then(data => setAlbums(data))
-      .catch(error => console.log(error));
+      .then((response) => response.json())
+      .then((data) => setAlbums(data))
+      .catch((error) => console.log(error));
   }, [userId]);
 
   const handleAlbumClick = (albumId) => {
-    setSelectedAlbum(albumId);
-    setStart(0);
-    setEnd(10);
-    fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`)
-      .then(response => response.json())
-      .then(data => setPhotos(data))
-      .catch(error => console.log(error));
-  }
+    if (selectedAlbum === albumId && showAlbums) {
+      setShowAlbums(false);
+      setSelectedAlbum(null);
+      setPhotos([]);
+    } else {
+      setSelectedAlbum(albumId);
+      setShowAlbums(true);
+      setStart(0);
+      setEnd(10);
+      fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`)
+        .then((response) => response.json())
+        .then((data) => setPhotos(data))
+        .catch((error) => console.log(error));
+    }
+  };
 
   const handleLoadMore = () => {
     setStart(start + 10);
     setEnd(end + 10);
-  }
+  };
 
   return (
     <div>
       <h2>Albums</h2>
       <ul>
-        {albums.map(album => (
-          <li key={album.id} className={selectedAlbum === album.id ? 'selected' : ''}>
-            <button onClick={() => handleAlbumClick(album.id)}>View</button>
-            {album.title}
+        {albums.map((album) => (
+          <li
+            key={album.id}
+            className={selectedAlbum === album.id ? "selected" : ""}
+          >
+            <button onClick={() => handleAlbumClick(album.id)}>
+              {selectedAlbum === album.id && showAlbums ? "Close" : "View"}
+            </button>
+            {selectedAlbum === album.id && showAlbums ? "" : `${album.title}`}
+
             {selectedAlbum === album.id && (
               <div className="photo-list">
-                {photos.slice(start, end).map(photo => (
-                  <img key={photo.id} src={photo.thumbnailUrl} alt={photo.title} />
+                <p>
+                  <strong>{album.title}</strong>
+                </p>
+                {photos.slice(start, end).map((photo) => (
+                  <img
+                    key={photo.id}
+                    src={photo.thumbnailUrl}
+                    alt={photo.title}
+                  />
                 ))}
-                {end < photos.length && <button onClick={handleLoadMore}>Load More</button>}
+                {end < photos.length && (
+                  <button onClick={handleLoadMore}>Load More</button>
+                )}
               </div>
             )}
           </li>
--- src/js/Posts.js
+++ src/js/Posts.js
@@ -1,46 +1,83 @@
 import React, { useState, useEffect } from "react";
+import "../css/posts.css"; // import the CSS file
 
 function Posts() {
   const [posts, setPosts] = useState([]);
   const [selectedPost, setSelectedPost] = useState(null);
   const [comments, setComments] = useState([]);
+  const [showComments, setShowComments] = useState(false);
+  const [showPostDetails, setShowPostDetails] = useState(false);
 
   useEffect(() => {
-    const user = JSON.parse(localStorage.getItem('user'));
+    const user = JSON.parse(localStorage.getItem("user"));
     const userId = user.id;
     fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
-      .then(response => response.json())
-      .then(data => setPosts(data))
-      .catch(error => console.log(error));
+      .then((response) => response.json())
+      .then((data) => setPosts(data))
+      .catch((error) => console.log(error));
   }, []);
 
   const handlePostClick = (postId) => {
-    setSelectedPost(postId);
-    fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
-      .then(response => response.json())
-      .then(data => setComments(data))
-      .catch(error => console.log(error));
-  }
+    if (selectedPost === postId && showComments) {
+      setShowComments(false);
+      setComments([]);
+      setSelectedPost(null);
+    } else {
+      setSelectedPost(postId);
+      setShowComments(true);
+      fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
+        .then((response) => response.json())
+        .then((data) => setComments(data))
+        .catch((error) => console.log(error));
+    }
+  };
+
+  const handleRowDoubleClick = (postId) => {
+    if (selectedPost === postId && showPostDetails) {
+      setShowPostDetails(false);
+    } else {
+      setSelectedPost(postId);
+      setShowPostDetails(true);
+    }
+  };
 
   return (
     <div>
       <h2>Posts</h2>
       <ul>
-        {posts.map(post => (
-          <li key={post.id} className={selectedPost === post.id ? 'selected' : ''}>
-            <button onClick={() => handlePostClick(post.id)}>View</button>
-            {post.title}
-            {selectedPost === post.id && (
+        {posts.map((post) => (
+          <li
+            key={post.id}
+            className={`post-item ${
+              selectedPost === post.id && showPostDetails ? "selected" : ""
+            }`}
+            onDoubleClick={() => handleRowDoubleClick(post.id)}
+          >
+            <button onClick={() => handlePostClick(post.id)}>
+              {selectedPost === post.id && showComments ? "Close" : "View"}
+            </button>
+            {selectedPost === post.id && (showPostDetails || showComments)
+              ? ""
+              : `${post.title}`}
+
+            {selectedPost === post.id && showComments && (
               <div>
                 <h3>Comments</h3>
                 <ul>
-                  {comments.map(comment => (
+                  {comments.map((comment) => (
                     <li key={comment.id}>
                       <strong>{comment.name}</strong>
                       <p>{comment.body}</p>
                     </li>
                   ))}
                 </ul>
+              </div>
+            )}
+            {selectedPost === post.id && showPostDetails && (
+              <div>
+                <h3>Post Details</h3>
+                <strong> {post.title}</strong>
+                <p>{post.body}</p>
               </div>
             )}
           </li>
