fa8a5373b07f84d947101a32a46793c5b96c974b
da077 <da077@DESKTOP-HV19KNK>
2023-05-09 12:40:10 +0300

    grid

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -6,38 +6,63 @@ import {
   Routes,
   Link,
   useHistory,
-} from 'react-router-dom';
-import Info from '../js/Info';
-import Album from '../js/Album';
-import Login from '../js/Login';
-import Posts from '../js/Posts';
-import Todos from '../js/Todos';
+} from "react-router-dom";
+import Info from "../js/Info";
+import Album from "../js/Album";
+import Login from "../js/Login";
+import Posts from "../js/Posts";
+import Todos from "../js/Todos";
+import "../css/application.css";
 
- function Application() {
-    const user = JSON.parse(localStorage.getItem('user'));
-  
-    return (
+function Application() {
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  return (
+    <div>
+      <h1 className="header">Welcome {user.name}!</h1>
+      <nav className="menuwBar">
+        <ul>
+          <li>
+            <Link to="/application/info">Info</Link>
+          </li>
+          <li>
+            <Link to="/application/todos">Todos</Link>
+          </li>
+          <li>
+            <Link to="/application/posts">Posts</Link>
+          </li>
+          <li>
+            <Link to="/application/albums">Albums</Link>
+          </li>
+          <li>
+            <Link to="/login" onClick={() => localStorage.removeItem("user")}>
+              Logout
+            </Link>
+          </li>
+        </ul>
+      </nav>
       <div>
-        <h1>Welcome {user.name}!</h1>
-        <nav>
-          <ul>
-          <li><Link to="/application/info">Info</Link></li>
-            <li><Link to="/application/todos">Todos</Link></li>
-            <li><Link to="/application/posts">Posts</Link></li>
-            <li><Link to="/application/albums">Albums</Link></li>
-            <li><Link to="/login" onClick={() => localStorage.removeItem('user')}>Logout</Link></li>
-          </ul>
-        </nav>
-        <div>
-        <Routes>
-            <Route path="/application/info" element={<Info />}/>
-            <Route path="/application/todos" element={<Todos />}/>
-            <Route path="/application/posts" element={<Posts />}/>
-          <Route path="/application/albums" element={<Album />}/>
-
-        </Routes>
+        <Routes className="content">
+          <Route className="info" path="/application/info" element={<Info />} />
+          <Route
+            className="todos"
+            path="/application/todos"
+            element={<Todos />}
+          />
+          <Route
+            className="posts"
+            path="/application/posts"
+            element={<Posts />}
+          />
+          <Route
+            className="albums"
+            path="/application/albums"
+            element={<Album />}
+          />
+                 
+        </Routes>
       </div>
     </div>
-            );
- }
-        export default Application
+  );
+}
+export default Application;
