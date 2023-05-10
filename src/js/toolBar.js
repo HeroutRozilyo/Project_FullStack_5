74ec501852ad682d57a24232d4fe639f2e6a6adb
da077 <da077@DESKTOP-HV19KNK>
2023-05-10 14:19:27 +0300

    גגג

--- src/js/toolBar.js
+++ src/js/toolBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "../css/toolBar.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
