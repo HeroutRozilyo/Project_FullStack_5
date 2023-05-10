0827b7da39920b1b509a84f2bf0ec9403d1f80d4
da077 <da077@DESKTOP-HV19KNK>
2023-05-10 11:27:23 +0300

    tool1

--- src/js/Layout.js
+++ src/js/Layout.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Header from "./header";
+import Toolbar from "./toolBar";
 import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
     <>
-      <Header />
+      <Toolbar />
       <Outlet />
     </>
   );
--- src/js/toolBar.js
+++ src/js/toolBar.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../css/toolBar.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../css/toolBar.css";
 
 function Toolbar() {
   return (
