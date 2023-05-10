b0e33d6ab251a9d30a04fb8c32cf725363d0a985
HeroutRozilyo <heroot12@gmail.com>
2023-05-10 13:05:12 +0300

    application

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Toolbar from '../js/toolBar.js';
 import Info from '../js/Info';
 import Album from '../js/Album';
@@ -10,21 +10,76 @@ import '../css/application.css';
 
 function Application() {
   const user = JSON.parse(localStorage.getItem('user'));
+  const [posts, setPosts] = useState([]);
+  const [albums, setAlbums] = useState([]);
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    // Fetch posts from the API
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((data) => setPosts(data.slice(0, 5))); // Limiting to first 5 posts
+
+    // Fetch albums from the API
+    fetch('https://jsonplaceholder.typicode.com/albums')
+      .then((response) => response.json())
+      .then((data) => setAlbums(data.slice(0, 5))); // Limiting to first 5 albums
+
+    // Fetch todos from the API
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then((response) => response.json())
+      .then((data) => setTodos(data.slice(0, 5))); // Limiting to first 5 todos
+  }, []);
 
   return (
+    
     <div>
       
       <h1 className="header">Welcome {user.name}!</h1>
-      <div className="content">
-        
-          <Routes>
-            <Route path="/application/info" element={<Info />} />
-            <Route path="/application/todos" element={<Todos />} />
-            <Route path="/application/posts" element={<Posts />} />
-            <Route path="/application/albums" element={<Album />} />
-            {/* Add other routes for different pages */}
-          </Routes>
-        
+      <div className="contentAP">
+        <div className="sectionAP" id='posts'>
+          <Link to="/application/posts">
+            <h2>Posts</h2>
+          </Link>
+          <ul>
+            {posts.map((post) => (
+              <li key={post.id}>{post.title}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="sectionAP" id='album'>
+          <Link to="/application/albums">
+            <h2>Albums</h2>
+          </Link>
+          <ul>
+            {albums.map((album) => (
+              <li key={album.id}>{album.title}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="sectionAP" id='todos'>
+          <Link to="/application/todos">
+            <h2>Todos</h2>
+          </Link>
+          <ul>
+            {todos.map((todo) => (
+              <li key={todo.id}>
+                {todo.completed ? (
+                  <span className="incomplete-task">
+                  <i className="fas fa-times"></i>
+                </span>
+                
+                ) : (
+                  <span className="completed-task">
+  <i className="fas fa-check"></i>
+</span>
+
+                )}
+                {todo.title}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
