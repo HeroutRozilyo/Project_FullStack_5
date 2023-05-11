63093bedc445c3dcda5d7983a37ca53e95babeab
HeroutRozilyo <heroot12@gmail.com>
2023-05-11 19:58:05 +0300

    photo

--- src/js/AlbumsItem.js
+++ src/js/AlbumsItem.js
@@ -1,20 +1,26 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import "../css/albumItem.css";
 
 function AlbumsItem() {
   const [photos, setPhotos] = useState([]);
   const [start, setStart] = useState(0);
   const [end, setEnd] = useState(10);
+  const [isLoading, setIsLoading] = useState(true);
   const { id } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${id}`)
       .then((response) => response.json())
-      .then((data) => setPhotos(data))
+      .then((data) => {
+        setPhotos(data);
+        setIsLoading(false);
+      })
       .catch((error) => console.log(error));
   }, [id]);
 
-  if (!photos) {
+  if (isLoading) {
     return <h1>Loading...</h1>;
   }
 
@@ -23,18 +29,29 @@ function AlbumsItem() {
     setEnd(end + 10);
   };
 
+  const handleBackClick = () => {
+    navigate("/application/albums");
+  };
+
   return (
     <div className="photo-list-container">
-      <div className="photo-list">
-        {photos.slice(start, end).map((photo) => (
-          <img
-            key={photo.id}
-            src={photo.thumbnailUrl}
-            alt={photo.title}
-            className="photo-item"
-          />
-        ))}
-      </div>
+      <button className="view-comments-button" onClick={handleBackClick}>
+        Back to Albums
+      </button>
+      {photos.length > 0 ? (
+        <div className="photo-list">
+          {photos.slice(start, end).map((photo) => (
+            <img
+              key={photo.id}
+              src={photo.thumbnailUrl}
+              alt={photo.title}
+              className="photo-item"
+            />
+          ))}
+        </div>
+      ) : (
+        <p className="loading-message">No photos found.</p>
+      )}
       {end < photos.length && (
         <div className="load-more">
           <button onClick={handleLoadMore} className="load-more-button">
